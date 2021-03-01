b92e83389294782fae771ef5e21ea4ba2855b50f
Kasander Hanssen <kasanderh@gmail.com>
2021-03-01 19:15:58 +0100

    Added images activity_hariov60.xml

---
+++ app/src/main/java/com/kasanderh/newcoffeeapp/Hariov60Activity.kt
@@ -0,0 +1,11 @@
+package com.kasanderh.newcoffeeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Hariov60Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hariov60)
+    }
+}
