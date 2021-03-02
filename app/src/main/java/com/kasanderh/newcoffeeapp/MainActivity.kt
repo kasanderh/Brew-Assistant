4d8a0cfaf894bf54c0517665bc53610adabbcb2c
Kasander Hanssen <kasanderh@gmail.com>
2021-03-02 17:48:14 +0100

    Added colors.xml Added actionbar Added label for actionbar Added onClickListener for Hariov60 button

--- app/src/main/java/com/kasanderh/newcoffeeapp/MainActivity.kt
+++ app/src/main/java/com/kasanderh/newcoffeeapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.kasanderh.newcoffeeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        onClickListeners()
+    }
+
+    private fun onClickListeners() {
+        card_view_v60.setOnClickListener {
+            val intent = Intent(this, Hariov60Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
