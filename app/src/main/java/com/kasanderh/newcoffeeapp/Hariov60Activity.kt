2a26b33a471f1a41f53726ebbe89470c8e5e5093
Kasander Hanssen <kasanderh@gmail.com>
2021-03-08 19:05:35 +0100

    Added new colors.xml Added background_fade.xml Changed background_card_view_main.xml

--- app/src/main/java/com/kasanderh/newcoffeeapp/Hariov60Activity.kt
+++ app/src/main/java/com/kasanderh/newcoffeeapp/Hariov60Activity.kt
@@ -2,10 +2,18 @@ package com.kasanderh.newcoffeeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hariov60.*
 
 class Hariov60Activity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hariov60)
+        setText()
+    }
+
+    private fun setText() {
+
     }
 }
