61880f08162c06bbe6309d9cfb89d7d1fd86933f
Kasander Hanssen <kasanderh@gmail.com>
2021-03-18 18:50:31 +0100

    Fixed activity_hariov60.xml Edited Strings for activity_hariov60.xml Stylized strings

--- app/src/main/java/com/kasanderh/newcoffeeapp/Hariov60Activity.kt
+++ app/src/main/java/com/kasanderh/newcoffeeapp/Hariov60Activity.kt
@@ -10,20 +10,20 @@ class Hariov60Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hariov60)
-        setText()
+//        setText()
     }
 
-    private fun setText() {
-        text_view_hario_title.text = "Hario V60"
-        text_view_hario_description.text = "Hario V60 is a pourover coffee maker"
-        text_view_hario_recipe.text = """
-            Step 1: asdasdasd
-            
-            Step 2: asdasdasd
-            
-            Step 3: asdasdasddaddsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsssssssssssssssssssssssssssssssss
-        """.trimIndent()
-
-
-    }
+//    private fun setText() {
+//        text_view_hario_title.text = "Hario V60"
+//        text_view_hario_description.text = "Hario V60 is a pourover coffee maker"
+//        text_view_hario_recipe.text = """
+//            Step 1: asdasdasd
+//
+//            Step 2: asdasdasd
+//
+//            Step 3: asdasdasddaddsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsssssssssssssssssssssssssssssssss
+//        """.trimIndent()
+//
+//
+//    }
 }
