7ddbce20bdd994edec8d7bee91a23e00e68d499e
Kasanderh <Kasanderh@gmail.com>
2021-04-06 12:35:43 +0200

    Edited activity_about.xml Edited about strings

--- app/src/main/java/com/kasanderh/newcoffeeapp/AboutActivity.kt
+++ app/src/main/java/com/kasanderh/newcoffeeapp/AboutActivity.kt
@@ -1,11 +1,75 @@
 package com.kasanderh.newcoffeeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.layout_bottom_bar.*
 
 class AboutActivity : AppCompatActivity() {
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        setupBottomSheet()
+        onClickListeners()
+    }
+
+    private fun setupBottomSheet() {
+        // Initializing bottomSheetBehavior
+        bottomSheetBehavior = BottomSheetBehavior.from(layout_bottom_sheet)
+
+        // OnClickListener for bottomSheetBehavior
+        bottomSheetBehavior.addBottomSheetCallback(
+            object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                }
+
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    // may need code here
+                }
+            }
+        )
+    }
+
+    private fun onClickListeners() {
+        // Change state when clicked
+        image_view_button_timer.setOnClickListener {
+            val state =
+                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED)
+                    BottomSheetBehavior.STATE_COLLAPSED
+                else
+                    BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetBehavior.state = state
+        }
+
+        //onClickListener for BottomSheet buttons
+        button_bottom_start.setOnClickListener {
+            chronometer_bottom_bar.base = SystemClock.elapsedRealtime()
+            chronometer_bottom_bar.start()
+        }
+
+        button_bottom_stop.setOnClickListener {
+            chronometer_bottom_bar.stop()
+        }
+
+        button_bottom_reset.setOnClickListener {
+            chronometer_bottom_bar.base = SystemClock.elapsedRealtime()
+        }
+
+        image_view_button_info.setOnClickListener {
+            Toast.makeText(this, "You are already on this page!", Toast.LENGTH_LONG).show()
+        }
+
+        image_view_button_home.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
