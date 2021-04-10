560dbe3c02dad6821e2c19cd9d458e1f0a293c76
Kasander Hanssen <kasanderh@gmail.com>
2021-04-10 16:20:06 +0200

    Added ScrollView to activity_aeropress, activity_french_press, activity_iced_coffee, activity_coffee_tips, activity_calculator Fixed onClickListeners for CoffeeTipsActivity Fixed marginBottom for all activities

--- app/src/main/java/com/kasanderh/newcoffeeapp/CalculatorActivity.kt
+++ app/src/main/java/com/kasanderh/newcoffeeapp/CalculatorActivity.kt
@@ -1,20 +1,46 @@
 package com.kasanderh.newcoffeeapp
 
+import android.content.Intent
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.View
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_calculator.*
+import kotlinx.android.synthetic.main.layout_bottom_bar.*
 import java.lang.Double.valueOf
 import kotlin.math.roundToInt
 
 class CalculatorActivity : AppCompatActivity() {
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+        setupBottomSheet()
         onClickListeners()
+    }
+
+    private fun setupBottomSheet() {
+        // Initializing bottomSheetBehavior
+        bottomSheetBehavior = BottomSheetBehavior.from(layout_bottom_sheet)
+
+        // OnClickListener for bottomSheetBehavior
+        bottomSheetBehavior.addBottomSheetCallback(
+            object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                }
+
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    // may need code here
+                }
+            }
+        )
     }
 
     private fun onClickListeners() {
@@ -128,6 +154,40 @@ class CalculatorActivity : AppCompatActivity() {
 //                }
 //            }
 
+        }
+
+        // Change state when clicked
+        image_view_button_timer.setOnClickListener {
+            val state =
+                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED)
+                    BottomSheetBehavior.STATE_COLLAPSED
+                else
+                    BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetBehavior.state = state
+        }
+
+        //onClickListener for BottomSheet buttons
+        button_bottom_start.setOnClickListener {
+            chronometer_bottom_bar.base = SystemClock.elapsedRealtime()
+            chronometer_bottom_bar.start()
+        }
+
+        button_bottom_stop.setOnClickListener {
+            chronometer_bottom_bar.stop()
+        }
+
+        button_bottom_reset.setOnClickListener {
+            chronometer_bottom_bar.base = SystemClock.elapsedRealtime()
+        }
+
+        image_view_button_info.setOnClickListener {
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+        }
+
+        image_view_button_home.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
 
