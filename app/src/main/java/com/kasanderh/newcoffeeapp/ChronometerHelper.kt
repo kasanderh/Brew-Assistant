bd70995d196863a5b3ef5d119d4e8987d97ab9f1
Kasander Hanssen <kasanderh@gmail.com>
2021-04-20 22:09:54 +0200

    Commented out code in the ChronometerHelper.kt to build up the Singleton class

--- app/src/main/java/com/kasanderh/newcoffeeapp/ChronometerHelper.kt
+++ app/src/main/java/com/kasanderh/newcoffeeapp/ChronometerHelper.kt
@@ -6,6 +6,7 @@ import android.widget.Chronometer
 object ChronometerHelper {
 
 //    abstract var chronometerHelper: ChronometerHelper
+
     init {
         println("Chronometer initialized")
         initializeChronometer()
@@ -13,44 +14,42 @@ object ChronometerHelper {
 
     private fun initializeChronometer() {
         TODO("Not yet implemented")
+        // initialize here
+
     }
 
-    lateinit var counter: Chronometer
-        var timeWhenStopped: Long = 0
+//    lateinit var counter: Chronometer
+//        var timeWhenStopped: Long = 0
 
 
-
-
-
-
-    public fun startChronometer() {
-        if (counter.isActivated) {
-            println("Stopwatch is already running!")
-        } else if (!counter.isActivated) {
-            if (timeWhenStopped == 0L) {
-                counter.text.toString()
-                counter.base = SystemClock.elapsedRealtime()
-                counter.start()
-            } else {
-                counter.base = SystemClock.elapsedRealtime() + timeWhenStopped
-                counter.start()
-            }
-        }
-    }
-
-    fun getValue(): Long {
-        return counter.base
-    }
-
-    fun stopChronometer() {
-        timeWhenStopped = counter.base - SystemClock.elapsedRealtime()
-        counter.stop()
-    }
-
-    fun resetChronometer() {
-        counter.base = SystemClock.elapsedRealtime()
-        timeWhenStopped = 0
-    }
+//    public fun startChronometer() {
+//        if (counter.isActivated) {
+//            println("Stopwatch is already running!")
+//        } else if (!counter.isActivated) {
+//            if (timeWhenStopped == 0L) {
+//                counter.text.toString()
+//                counter.base = SystemClock.elapsedRealtime()
+//                counter.start()
+//            } else {
+//                counter.base = SystemClock.elapsedRealtime() + timeWhenStopped
+//                counter.start()
+//            }
+//        }
+//    }
+//
+//    fun getValue(): Long {
+//        return counter.base
+//    }
+//
+//    fun stopChronometer() {
+//        timeWhenStopped = counter.base - SystemClock.elapsedRealtime()
+//        counter.stop()
+//    }
+//
+//    fun resetChronometer() {
+//        counter.base = SystemClock.elapsedRealtime()
+//        timeWhenStopped = 0
+//    }
 
 
 }
