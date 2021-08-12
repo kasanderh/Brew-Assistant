6c3b4759b7ae941d4c558ed1e52ed47944d6416a
Kasander Hanssen <kasanderh@gmail.com>
2021-08-12 15:27:45 +0200

    Setup tests in MainActivityTest.kt and CalculatorActivityTest.kt Added functions for testing

--- app/src/androidTest/java/com/kasanderh/newcoffeeapp/CalculatorActivityTest.kt
+++ app/src/androidTest/java/com/kasanderh/newcoffeeapp/CalculatorActivityTest.kt
@@ -3,6 +3,7 @@ package com.kasanderh.newcoffeeapp
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 
@@ -11,8 +12,25 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class CalculatorActivityTest {
 
-    @get:Rule
+    @Rule
     var activityRule: ActivityScenarioRule<CalculatorActivity> = ActivityScenarioRule(CalculatorActivity::class.java)
+
+    @Test
+    fun testCalculatorInputWaterGetCoffee() {
+
+
+    }
+
+    @Test
+    fun testCalculatorInputCoffeeGetWater() {
+
+
+    }
+
+    @Test
+    fun clickClearButton_ClearsViews() {
+
+    }
 
 
 
--- app/src/androidTest/java/com/kasanderh/newcoffeeapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/kasanderh/newcoffeeapp/MainActivityTest.kt
@@ -1,6 +1,11 @@
 package com.kasanderh.newcoffeeapp
 
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
@@ -8,17 +13,25 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 
-
-
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    @get:Rule
-    var activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+    @Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    onView(withId(R.id.)).perform(click()).check(matches())
+    fun clickCalculatorButton_OpensCalculatorActivity() {
+        // Clicks the Calculator button in the MainActivity
+        onView(withId(R.id.card_view_calculator)).perform(click())
 
+        // Checks that the CalculatorActivity is open by checking that the input field is blank
+        onView(withId(R.id.text_edit_box_water)).check(matches(withText("")))
+
+
+
+    }
 
 
 }
+
