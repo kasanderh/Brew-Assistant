b0ce1f5d8d75234ff96678a3f04595019177f26c
Kasander Hanssen <kasanderh@gmail.com>
2021-08-18 18:22:59 +0200

    Added tests for MainActivity buttons to check that they open the correct activity

--- app/src/androidTest/java/com/kasanderh/newcoffeeapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/kasanderh/newcoffeeapp/MainActivityTest.kt
@@ -27,10 +27,52 @@ class MainActivityTest {
 
         // Checks that the CalculatorActivity is open by checking that the input field is blank
         onView(withId(R.id.text_edit_box_water)).check(matches(withText("")))
+    }
 
+    @Test
+    fun clickHarioV60Button_OpensHarioV60Activity() {
+        onView(withId(R.id.card_view_v60)).perform(click())
+
+        // Checks that the HarioV60Activity is open by checking that the text_view_hario_title is showing
+        // the correct string
+        onView(withId(R.id.text_view_hario_title)).check(matches(withText(R.string.v60_description)))
+    }
+
+    @Test
+    fun clickAeropressButton_OpensAeropressActivity() {
+        onView(withId(R.id.card_view_aeropress)).perform(click())
+
+        onView(withId(R.id.text_view_aeropress_description)).check(matches(withText(R.string.aeropress_description)))
 
 
     }
+
+    @Test
+    fun clickFrenchPressButton_OpensFrenchPressActivity() {
+        onView(withId(R.id.card_view_french_press)).perform(click())
+
+        onView(withId(R.id.text_view_french_press_description)).check(matches(withText(R.string.french_press_description)))
+
+
+    }
+
+    @Test
+    fun clickIcedCoffeeButton_OpensIcedCoffeeActivity() {
+        onView(withId(R.id.card_view_iced_coffee)).perform(click())
+
+        onView(withId(R.id.text_view_iced_coffee_description)).check(matches(withText(R.string.iced_coffee_description)))
+
+
+    }
+    @Test
+    fun clickCoffeeTipsButton_OpensCoffeeTipsActivity() {
+        onView(withId(R.id.card_view_coffee_tips)).perform(click())
+
+        onView(withId(R.id.text_view_coffee_tips_description)).check(matches(withText(R.string.coffee_tips_description)))
+
+
+    }
+
 
 
 }
